579bc9ad561a5a03e70606a71dec42853f4c2266
Roberto Floris <rmfloris@gmail.com>
2018-04-12 07:47:20 +0200

    Updated navigation styling

--- src/Components/Navigation/style.js
+++ src/Components/Navigation/style.js
@@ -10,7 +10,9 @@ export const NavContainer = styled.ul`
   position: relative;
   z-index: 1000;
   align-self: auto;
-  padding: 0;
+  border-bottom: ${props => (props.isHome ? "none" : "#e4e4e4 1px dashed")};
+  padding-bottom: 10px;
+  margin-bottom: 0px;
 `;
 
 export const StyledItem = styled.li`
@@ -20,7 +22,6 @@ export const StyledItem = styled.li`
   text-transform: uppercase;
   padding: 10px 10px 10px 10px;
   color: ${props => (props.isHome ? "white" : "black")};
-  border-bottom: ${props => (props.isHome ? "none" : "#e4e4e4 1px dashed")};
 `;
 
 export const Icon = styled.svg``;
