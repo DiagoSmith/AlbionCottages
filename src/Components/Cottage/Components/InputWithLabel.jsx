2bb1bf28c147b7527b680d9f2a3192c92eb4ff31
Roberto Floris <rmfloris@gmail.com>
2018-04-28 17:14:16 +0200

    combined input and label into one component

---
+++ src/Components/Cottage/Components/InputWithLabel.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { string, func } from "prop-types";
+
+import { StyledLabel, StyledInput, StyledContainer } from "../style";
+
+const nameToType = { name: "text", email: "email", phonenumber: "number" };
+const nameToLabel = {
+  name: "Name",
+  email: "Email",
+  phonenumber: "Phone Number"
+};
+const nameToPlaceHolder = {
+  name: "John Doe",
+  email: "johndoe@gmail.com",
+  phonenumber: "+447723739..."
+};
+
+const InputWithLabel = ({ name, onChange, value }) => {
+  return (
+    <StyledContainer>
+      <StyledLabel for={name}> {nameToLabel[name]} </StyledLabel>
+      <StyledInput
+        name={name}
+        type={nameToType[name]}
+        value={value}
+        placeholder={nameToPlaceHolder[name]}
+        require
+        onChange={e => onChange(e, name)}
+      />
+    </StyledContainer>
+  );
+};
+
+InputWithLabel.propTypes = {
+  name: string.isRequired,
+  onChange: func.isRequired,
+  value: string.isRequired
+};
+
+export default InputWithLabel;
